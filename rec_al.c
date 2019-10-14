4f6b159d8152621bca800c64fb31ccf69bef1dac
Ayush Bhat <tfeayush@gmail.com>
2019-10-14 20:10:26 +0530

    init

---
+++ rec_al.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define STR_LEN 100
+
+char str[STR_LEN];
+
+
+char * get_new_string() {
+    fgets(str, STR_LEN, stdin);
+    return str;
+}
+
+char * f_string(char *s) {
+    int i, j;
+    char *r, *ss;
+    r = s;
+    ss = s;
+    for(j = 0, i = 0; j < strlen(s); j++) {
+        if(*(ss + j) != ' ') {
+            *(r + (i++)) = *(ss + j);
+        }
+    }
+    *(r+i) = '\0';
+    return r;
+} 
+
+
+int main() {
+    char *ch = get_new_string();
+    puts(f_string(ch));
+
+    return 0;
+}
