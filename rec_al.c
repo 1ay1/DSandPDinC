2cf7b8e49f004301f678dd86a11a8507924ef772
Ayush <Ayush>
2019-10-14 23:07:40 +0530

    init

--- rec_al.c
+++ rec_al.c
@@ -7,6 +7,10 @@
 
 #define STR_LEN 100
 
+int is_factor(char str[], int length, int *ppos);
+int is_term(char str[], int length, int *ppos);
+int is_expr(char str[], int length, int *ppos);
+
 char str[STR_LEN];
 
 
@@ -29,14 +33,52 @@ char * f_string(char *s) {
     return r;
 } 
 
-void nothing(){
-    
+char get_symbol(char sym[], int length, int *ppos) {
+    char c;
+
+    if( *ppos < length) c = str[*ppos];
+    else c =  ' ';
+
+    (*ppos)++;
+    return c;
+}
+
+int is_factor(char str[], int length, int *ppos) {
+    int c;
+    if((c = get_symbol(str, length, ppos)) != '(') return (isalpha(c));
+    return (is_expr(str, length, ppos) && (get_symbol(str, length, ppos) == ')') ); 
+}
+
+int is_term(char str[], int length, int *ppos) {
+    if(is_factor(str, length, ppos) == 0) return 0;
+    if(get_symbol(str, length, ppos) != '*') {
+        (*ppos)--;
+        return 1;
+    }
+    return(is_factor(str, length, ppos));
 }
 
 
+
+
+int is_expr(char str[], int length, int * ppos) {
+    if(is_term(str, length, ppos) == 0) return 0;
+    if(get_symbol(str, length, ppos) != '+') {
+        (*ppos)--;
+        return 1;
+    }
+    return (is_term(str, length, ppos));
+}
+
 int main() {
     char *ch = get_new_string();
-    puts(f_string(ch));
+    int pos = 0;
+    if((is_expr(ch, strlen(ch), &pos) == 1) && pos <= strlen(ch)) {
+        printf("Valid Expression!\n");
+    }
+    else {
+        puts("Invalid Expression!");
+    }
 
     return 0;
 }
