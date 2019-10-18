2661121dcef125717d1cb3ff2340ebe14713fc37
Ayush Bhat <tfeayush@gmail.com>
2019-10-18 18:35:23 +0530

    init

--- avg.c
+++ avg.c
@@ -57,11 +57,66 @@ int find(char str[]) {
     temp = between_substring(str, m+1, length - m - 1);
     if(n == 0) return 0; // no valid second operand, so not a valid prefix exp
 
-    return(m+n+1) // plus one to add the starting operator that we removed
+    return(m+n+1); // plus one to add the starting operator that we removed
 }
 
+// the convert function that uses find()
+void convert (char prefix[], char postfix[]) {
+    char *opnd1, *opnd2; // buffers for the two operands
+    char post1[MAXLENGTH], post2[MAXLENGTH]; // buffers for convered postfix strings from the operands
 
+    char *temp; //usual temp buffer
+    char op[2]; //buffer for op
+
+    int length;
+    int i, j, m, n;
+
+    if((length = strlen(prefix)) == 1) {
+        if(isalpha(prefix[0])) {
+            postfix[0] = prefix[0];
+            postfix[1] = '\0';
+            return;
+        }
+        else {
+            printf("\n Illegal Prefix string!\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // the prefix string is longer than one single char
+        // THE RECURSIVE PART
+
+        // the the operator in op
+        op[0] = prefix[0];
+        op[1] = '\0';
+        temp = between_substring(prefix, 1, length -1); 
+        m = find(temp); // get first operand
+        temp = between_substring(prefix, m+1, length - m - 1); // get second operand
+        n = find(temp); // get second operand
+
+        if((op[0] != '+' && op[0] != '-' && op[0] != '*' && op[0] != '/') || (m == 0) || (n == 0)
+        || (m + n + 1  != length)) {
+            printf("Invalid String");
+            exit(EXIT_FAILURE);
+        }
+
+        opnd1 = between_substring(prefix, 1 , m);
+        opnd2 = between_substring(prefix, m+1, n);
+        convert(opnd1, post1);
+        convert(opnd2, post2);
+        strcat(post1, post2);
+        strcat(post1, op);
+        postfix = between_substring(post1, 0, length);
+    }
+}
 
 int main() {
 
+    char prestr[MAXLENGTH], poststr[MAXLENGTH];
+    fgets(prestr, MAXLENGTH, stdin);
+    
+    convert(prestr, poststr);
+
+    puts(poststr);
+
+    return 0;
 }
